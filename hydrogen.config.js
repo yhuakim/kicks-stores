947b54c35802702d7b98b0da4c836e0357fc90f9
ARINZE JOACHIM OJILI <47617723+yhuakim@users.noreply.github.com>
2022-11-15 21:35:15 +0100

    added env

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from '@shopify/hydrogen/config';
 
 export default defineConfig({
   shopify: {
-    storeDomain: 'kicks-stores.myshopify.com',
-    storefrontToken: '7c9658b2162a945f2b57b438b79221b3',
+    storeDomain: process.env.DOMAIN,
+    storefrontToken: process.env.TOKEN,
     storefrontApiVersion: '2022-07',
   },
 });
